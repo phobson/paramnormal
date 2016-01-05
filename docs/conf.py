c742738794e20d0c9c3b3337c387c33bf73e3f46
Paul Hobson <pmhobson@gmail.com>
2016-01-05 11:45:05 -0800

    clean up doc configurations

--- docs/conf.py
+++ docs/conf.py
@@ -18,35 +18,28 @@ import os
 import shlex
 import sphinx
 
+source_suffix = ['.rst']
+
+numpydoc_show_class_members = False
 autodoc_member_order = 'bysource'
-
-from unittest import mock
-
-MOCK_MODULES = ['scipy', 'scipy.stats']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-
-# -- General configuration ------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+sys.path.insert(0, os.path.abspath('sphinxext'))
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'plot_generator',
+    'plot_directive',
     'numpydoc',
+    'ipython_directive',
+    'ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
