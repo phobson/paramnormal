748ff47a5a13a70df98376544aa7aaa891a950c1
Paul Hobson <pmhobson@gmail.com>
2016-01-23 15:33:09 -0800

    rebuilt notebooks

--- activity-1.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import seaborn
-import paramnormal
-clean_bkgd = {'axes.facecolor': 'none', 'figure.facecolor': 'none'}
-seaborn.set(style='ticks', rc=clean_bkgd)
-norm_dist = paramnormal.normal(μ=5.4, σ=2.5)
-ax = paramnormal.activity.plot(norm_dist)
--- activity-3.py
+++
@@ -1,2 +0,0 @@
-ax = paramnormal.activity.plot('normal', data=data, distplot=True)
-ax.legend(loc='upper left')
