ccc68e4df9e3e3d43aa196edc73b3426a779c2cc
Paul Hobson <pmhobson@gmail.com>
2016-04-24 22:46:23 -0700

    build with new plotting

--- api/activity-2.py
+++
@@ -1,3 +0,0 @@
-paramnormal.utils.seed(0)
-data = paramnormal.activity.random('normal', μ=5.4, σ=2.5, shape=(37))
-ax = paramnormal.activity.plot('normal', data=data)
--- api/activity-4.py
+++
@@ -1,9 +0,0 @@
-paramnormal.utils.seed(0)
-data = paramnormal.activity.random('lognormal', μ=0.75, σ=1.2, shape=125)
-logdata = numpy.log10(data)
-line_opts = dict(color='firebrick', lw=3.5, label='Fit PDF')
-distplot_opts = dict(rug=True, kde=False, norm_hist=True)
-ax = paramnormal.activity.plot('lognormal', data=data, distplot=True,
-                               xscale='log', pad=0.01,
-                               line_opts=line_opts,
-                               distplot_opts=distplot_opts)
