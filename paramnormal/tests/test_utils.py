1f1675713249579b2b31ff9fd3381aa93de17f3a
Paul Hobson <pmhobson@gmail.com>
2016-05-25 09:10:36 -0700

    update utils to pytest
    also rename _pop_none -> _remove_nones

--- paramnormal/tests/test_utils.py
+++ paramnormal/tests/test_utils.py
@@ -1,38 +1,29 @@
 # -*- coding: utf-8 -*-
 import numpy
 
-import nose.tools as nt
-from numpy.random import seed
-import numpy.testing as nptest
+import pytest
 
 from paramnormal import dist, utils
 
 
-def test_greco_deco():
-    d1 = dist.normal._process_args(mu=1, sigma=2)
-    d2 = dist.normal._process_args(μ=1, σ=2)
-    expected = dict(loc=1, scale=2)
-    nt.assert_dict_equal(d1, expected)
-    nt.assert_dict_equal(d2, expected)
-
-    d1 = dist.beta._process_args(alpha=1, beta=2)
-    d2 = dist.beta._process_args(α=1, β=2)
-    expected = {'a': 1, 'b': 2, 'loc': 0, 'scale': 1}
-    nt.assert_dict_equal(d1, expected)
-    nt.assert_dict_equal(d2, expected)
-
-    d1 = dist.gamma._process_args(k=1, theta=2)
-    d2 = dist.gamma._process_args(k=1, θ=2)
-    expected = {'a': 1, 'loc': 0, 'scale': 2}
-    nt.assert_dict_equal(d1, expected)
-    nt.assert_dict_equal(d2, expected)
+@pytest.mark.parametrize(('distro', 'distargs', 'expected'), [
+    (dist.normal, dict(mu=1, sigma=2),  dict(loc=1, scale=2)),
+    (dist.normal, dict(μ=1, σ=2),  dict(loc=1, scale=2)),
+    (dist.beta, dict(alpha=1, beta=2), dict(a=1, b=2, loc=0, scale=1)),
+    (dist.beta, dict(α=1, β=2), dict(a=1, b=2, loc=0, scale=1)),
+    (dist.gamma, dict(k=1, theta=2), dict(a=1, loc=0, scale=2)),
+    (dist.gamma, dict(k=1, θ=2), dict(a=1, loc=0, scale=2)),
+])
+def test_greco_deco(distro, distargs, expected):
+    result = distro._process_args(**distargs)
+    assert result == expected
 
 
-def test__pop_none():
-    expected_no_Nones = dict(a=1, b=2, c=3)
-    expected_some_Nones = dict(a=1, c=3)
-    expected_all_Nones = dict()
-
-    nt.assert_dict_equal(utils._pop_none(a=1, b=2, c=3), expected_no_Nones)
-    nt.assert_dict_equal(utils._pop_none(a=1, b=None, c=3), expected_some_Nones)
-    nt.assert_dict_equal(utils._pop_none(a=None, b=None, c=None), expected_all_Nones)
+@pytest.mark.parametrize(('a', 'b', 'c', 'expected'), [
+    (1,    2,    3,    dict(a=1, b=2, c=3)),
+    (1,    None, 3,    dict(a=1, c=3)),
+    (None, None, None, dict()),
+])
+def test__remove_nones(a, b, c, expected):
+    result = utils._remove_nones(a=a, b=b, c=c)
+    assert result == expected
